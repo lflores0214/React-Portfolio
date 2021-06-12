58931d17f2008303e2596951026f1f8a6168df2d
Luis <lflores02142@gmail.com>
2021-06-11 18:43:11 -0700

    navbar now functional

--- src/components/nav/navbar.styled.js
+++ src/components/nav/navbar.styled.js
@@ -4,7 +4,7 @@ export const NavbarWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-evenly;
+  justify-content: center;
   position: fixed;
   height: 97.85vh;
   width: 150px;
@@ -14,9 +14,12 @@ export const NavbarWrapper = styled.div`
     color: #45505b;
     text-decoration: none;
     font-size: 16px;
+    line-height: 4ch;
+    width: 100%;
+    text-align: center;
   }
-
   .active {
     color: #0563bb;
+    text-decoration: underline;
   }
 `;
